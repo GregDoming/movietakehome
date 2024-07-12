747f5500de4fab278a6ae3a6bc3f53fe4d9bb231
gregdoming <greg.domingue1@gmail.com>
2024-07-12 15:07:46 -0700

    one final message

--- movie_search/src/utils/urlGeneratorHelper.ts
+++ movie_search/src/utils/urlGeneratorHelper.ts
@@ -4,6 +4,16 @@ interface urlOptions {
     includeAdult?: boolean;
 }
 
+/**
+ * Edits url and has optional parameters.
+ *
+ * @param {string} query - The search query for fetching movies.
+ * @param {number} [page=1] - The page number for pagination (default is 1).
+ * @param {boolean} [includeAdult=false] - Whether to include adult movies in the results (default is false).
+ * @returns {Promise<Object>} A promise that resolves to the movie data.
+ */
+
+
 export const movieUrlEditor = ({ query, page = 1, includeAdult = false }: urlOptions) => {
     if (page <= 0) page = 1;
     let actualPage = Math.ceil(page / 2);
